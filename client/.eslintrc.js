76390483a8f319011974b718332db2889019c414
karlpawlowicz <karlpawlowicz@gmail.com>
2022-08-04 14:22:44 -0400

    fix: fix eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,21 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  extends: ['plugin:react/recommended', 'standard', 'eslint:recommended', 'plugin:react/jsx-runtime'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins: ['react'],
-  rules: {}
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    extends: [
+        'plugin:react/recommended',
+        'standard',
+        'eslint:recommended',
+        'plugin:react/jsx-runtime',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {},
 }
