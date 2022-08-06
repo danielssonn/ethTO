72afb86e7d135112f8587edefb6d76f5448db7d4
Matt Grannary <me@mattgrannary.com>
2022-08-06 10:57:43 -0400

    chore: update swing sdk

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    define: {
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        'process.env.SWING_API_URL': JSON.stringify(''),
-        'process.env.SDK_SENTRY_DSN': JSON.stringify(''),
-    },
     plugins: [react()],
 })
