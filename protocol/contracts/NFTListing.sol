9503835c70d2b663960759ef73e2f5e5c7bba944
Matt Grannary <me@mattgrannary.com>
2022-08-09 10:15:13 -0400

    feat: revert to original market

--- protocol/contracts/NFTListing.sol
+++ protocol/contracts/NFTListing.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import './Payment.sol';
-import './Collateral.sol';
-import './Rental.sol';
+import "./Payment.sol";
+import "./Collateral.sol";
+import "./Rental.sol";
 
 struct NFTListing {
-    address nftAddress;
-    uint256 tokenId;
     address lender;
     uint256 maximumEndTime;
     uint256 createTime;
--- protocol/scripts/dev.ts
+++ protocol/scripts/dev.ts
@@ -6,22 +6,13 @@ import senderJson from '../artifacts/contracts/SendAckSender.sol/SendAckSender.j
 
 async function main() {
     await createAndExport({
-        chains: ['Polygon', 'Avalanche'],
+        chains: ['Polygon', 'Moonbeam'],
         async callback(network, info) {
             console.log(info)
             const deployerWallet = new ethers.Wallet(
                 network.userWallets[0]._signingKey().privateKey,
                 network.provider
             )
-
-            const weth = await network.deployToken(
-                'WETH',
-                'aWETH',
-                18,
-                BigInt(100_000_000e18)
-            )
-
-            console.log('WETH', weth.address)
 
             const Sender = new ethers.ContractFactory(
                 senderJson.abi,
@@ -76,14 +67,8 @@ async function main() {
                 nft.address,
                 0,
                 Math.round(Date.now() / 1000) + 60 * 60 * 24 * 5,
-                {
-                    paymentToken: weth.address,
-                    pricePerDay: ethers.utils.parseEther('0.01'),
-                },
-                {
-                    collateralToken: weth.address,
-                    collateralAmount: ethers.utils.parseEther('2'),
-                }
+                ethers.utils.parseEther('0.01'),
+                ethers.utils.parseEther('2')
             )
 
             await tx1.wait()
@@ -92,14 +77,8 @@ async function main() {
                 nft.address,
                 1,
                 Math.round(Date.now() / 1000) + 60 * 60 * 24 * 5,
-                {
-                    paymentToken: weth.address,
-                    pricePerDay: ethers.utils.parseEther('0.01'),
-                },
-                {
-                    collateralToken: weth.address,
-                    collateralAmount: ethers.utils.parseEther('2'),
-                }
+                ethers.utils.parseEther('0.01'),
+                ethers.utils.parseEther('2')
             )
 
             await tx2.wait()
@@ -108,14 +87,8 @@ async function main() {
                 nft.address,
                 2,
                 Math.round(Date.now() / 1000) + 60 * 60 * 24 * 5,
-                {
-                    paymentToken: weth.address,
-                    pricePerDay: ethers.utils.parseEther('0.01'),
-                },
-                {
-                    collateralToken: weth.address,
-                    collateralAmount: ethers.utils.parseEther('2'),
-                }
+                ethers.utils.parseEther('0.01'),
+                ethers.utils.parseEther('2')
             )
 
             await tx3.wait()
@@ -124,14 +97,8 @@ async function main() {
                 nft.address,
                 3,
                 Math.round(Date.now() / 1000) + 60 * 60 * 24 * 5,
-                {
-                    paymentToken: weth.address,
-                    pricePerDay: ethers.utils.parseEther('0.01'),
-                },
-                {
-                    collateralToken: weth.address,
-                    collateralAmount: ethers.utils.parseEther('2'),
-                }
+                ethers.utils.parseEther('0.01'),
+                ethers.utils.parseEther('2')
             )
 
             await tx4.wait()
